41d3ad6eeb0f73626a52851444513beb94cc9ba2
james clarke <jkc200013@gmail.com>
2020-02-03 21:08:00 +0000

    removed builds

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,29 @@
 public class Main {
+
+    public static class Test implements Runnable{
+        public void run(){
+            try {
+                while (true){
+                    System.out.println("running!");
+                    // Thread.yield()
+                }
+            }catch (Exception e){
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Thread t = new Thread(new Main.Test());
+        t.start();
+        System.out.println("starting main!");
+        try{
+            t.interrupt();
+        }catch(Exception e){
+            System.out.println("stopped from main!");
+        }
+    }
+
     public static String lol(){
         return "lol";
     }
---
+++ src/main/java/platform/Task.java
@@ -0,0 +1,14 @@
+package platform;
+
+/**
+ * A task is a scheduled piece of code that can be started, stopped etc
+ * */
+public class Task implements Runnable{
+    public void start(){
+
+    }
+
+    public void run(){
+
+    }
+}
